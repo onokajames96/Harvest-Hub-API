547680313d4335c082a76aa8c0f24228648f9d60
maurice <maurice.nzai@gmail.com>
2024-05-23 08:08:35 +0000

    Connecting to Mongo DB and Running the Server

---
+++ server.js
@@ -0,0 +1,15 @@
+const app = require('./app');
+const connectDB = require('./config/db');
+
+const PORT = process.env.PORT || 3000;
+
+// Connect to MongoDB and Start Server
+connectDB().then(() => {
+	app.listen(PORT, () => {
+		console.log(`Server running on port ${PORT}...!!!`);
+	});
+}).catch(err => {
+	console.error("Failed to Connect to MongoDB...!!!", err);
+	process.exit(1);
+});
+
