12cb14111e17a387ad416ac283116c3d2848cb76
onokajames96 <jamesonoka996@gmail.com>
2024-05-25 16:41:02 -0400

    working on order routes

---
+++ routes/orderRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const orderController = require('../controllers/orderController');
+const auth = require('../middleware/auth');
+
+// Create the order
+router.post('/create', auth, orderController.createOrder);
+
+// Get user orders
+router.get('/:userId', auth, orderController.getUserOrders);
+
+// Get single order
+router.get('/order/:orderId', auth, orderController.getOrder);
+
+// Delete the order
+router.delete('/delete/:orderId', auth, orderController.deleteOrder);
+
+module.exports = router;
