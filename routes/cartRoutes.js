7c3023e738ce5676188000209a17c0bb7a16ee5b
onokajames96 <jamesonoka996@gmail.com>
2024-05-25 16:31:31 -0400

    working on cart routes.js file

---
+++ routes/cartRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const cartController = require('../controllers/cartController');
+const auth = require('../middleware/auth');
+
+// Add item to cart
+router.post('/add', auth, cartController.addToCart);
+
+// Remove item from cart
+router.post('/remove', auth, cartController.removeFromCart);
+
+// Get user cart
+router.get('/:userId', auth, cartController.getUserCart);
+
+// Delete the cart
+router.post('/delete', auth, cartController.deleteCart);
+
+module.exports = router;
