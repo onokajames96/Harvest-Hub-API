334c04e56195f2aecc433a3a1686961d9ad4bcca
maurice <maurice.nzai@gmail.com>
2024-05-28 13:35:15 +0000

    Modify function to connect to local MongoDB if connection to Atlas Fails

--- config/db.js
+++ config/db.js
@@ -3,14 +3,26 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+const atlasUri = process.env.MONGODB_ATLAS_URI;
+const localUri = process.env.MONGODB_LOCAL_URI;
+
 const connectDB = async () => {
-	try{
-		const conn = await mongoose.connect(process.env.MONGO_URI);
-		console.log("MongoDB Connection Successfull...!");
-	} catch (error) {
-		console.error("Error connecting to MongoDB...!", error.message);
-		process.exit(1);
-	}
-};
+	try {
+    // Try connecting to MongoDB Atlas
+    await mongoose.connect(atlasUri);
+    console.log("Connected successfully to MongoDB Atlas");
+  } catch (err) {
+    console.error("Failed to connect to MongoDB Atlas, " +
+	    "attempting to connect to local MongoDB");
+    try {
+      // Fallback to local MongoDB
+      await mongoose.connect(localUri);
+      console.log("Connected successfully to local MongoDB");
+    } catch (localErr) {
+      console.error("Failed to connect to local MongoDB", localErr);
+      throw localErr; // If both connections fail, throw an error
+    }
+  }
+}
 
 module.exports = connectDB;
