7771da74886f9830999572b37714eb6de1f0aaaf
maurice <maurice.nzai@gmail.com>
2024-05-29 18:41:50 +0000

    Added a try-catch block to handle any errors that may occur during the email sending process

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,25 +3,29 @@ const nodemailer = require('nodemailer');
 require('dotenv').config(); // Ensure environment variables are loaded
 
 const sendEmail = async (options) => {
-	const transporter = nodemailer.createTransport({
-		service: 'Gmail',
-		host: 'smtp.gmail.com',
-		port: 465,
-		secure: true,
-		auth: {
-			user: process.env.EMAIL_USERNAME,
-			pass: process.env.EMAIL_PASSWORD
-		}
-	});
+	try{
+		const transporter = nodemailer.createTransport({
+			service: 'Gmail',
+			host: 'smtp.gmail.com',
+			port: 465,
+			secure: true,
+			auth: {
+				user: process.env.EMAIL_USERNAME,
+				pass: process.env.EMAIL_PASSWORD
+			}
+		});
 
-	const mailOptions = {
-		from: 'harvesthub2004@gmail.com',
-		to: options.to,
-		subject: options.subject,
-		text: options.text
-	};
+		const mailOptions = {
+			from: 'harvesthub2004@gmail.com',
+			to: options.to,
+			subject: options.subject,
+			text: options.text
+		};
 
-	await transporter.sendMail(mailOptions);
+		await transporter.sendMail(mailOptions);
+	}catch (error) {
+		console.error('Error sending email:', error);
+	}
 };
 
 module.exports = sendEmail;
